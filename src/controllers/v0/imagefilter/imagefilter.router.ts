0e10855b3ff9aec38a9eae8cf43054b7a4677fef
Eduardo <eduardo.rondon@apstem.com>
2021-01-28 20:16:58 +0100

    feat: imagefilter function

--- src/controllers/v0/imagefilter/imagefilter.router.ts
+++ src/controllers/v0/imagefilter/imagefilter.router.ts
@@ -3,27 +3,67 @@ import {filterImageFromURL, deleteLocalFiles} from '../../../util/util';
 
 const router: Router = Router();
 
- // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
-  /**************************************************************************** */
+/**
+ * @description Function that transform the sendFile method of Response to a promise based 
+ * @since   0.0.1
+ * @access  private
+ * @param   {Response}  response
+ * @returns {Promise<string>}  response containing the string path
+*/
+async function promiseSendFile(response:Response,path:string): Promise<string> {
+  return new Promise( (resolve,reject) => {
+      response.sendFile(path, (error) =>{
+          if(error){
+            reject(error);
+          }else{
+            resolve(path);
+          }
+      });
+  });
+}
+/**
+ * @description Function that validates url with regex (From stakoverflow question id#5717093)
+ * @since   0.0.1
+ * @access  private
+ * @param   {string} url - the url to test
+ * @returns {boolean}  true when the url is correctly validated
+*/
+function validateURL(url:string):boolean {
+  var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+  return !!pattern.test(url);
+}
 
-  //! END @TODO1
-
-
-// Get all feed items
+// Filter image endpoint
 router.get('/', async (req: Request, res: Response) => {
-    const imageUrl: String = req.query.image_url;
+    const imageUrl:string = req.query.image_url as string;
+
+    let responseError:object = {
+      success: false,
+      status: 422,
+      type: "about:blank",
+      title: 'Wrong request',
+      detail: 'Request must contains a valid URL as image_url query parameter',
+      instance: "about:blank"
+    }
+
+    if (!validateURL(imageUrl)){
+      res.status(422).send(responseError);
+    }else {
+
+      filterImageFromURL(imageUrl)
+      .then(path => promiseSendFile(res,path))
+      .then( path => deleteLocalFiles([path]) )
+      .catch( error => res.status(500).send('Internal Server Error'));
+
+    }
+
+
 });
 
 export const ImageFilterRouter: Router = router;
